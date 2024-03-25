ab6110dd48531f71f0c94e600cd9b8491f58d948
Your Name <ansonchung2012@gmail.com>
2024-03-25 23:52:38 +0800

    Editted Edit Distance scripts

--- Leetcode/72-EditDistance.py
+++ Leetcode/72-EditDistance.py
@@ -5,22 +5,26 @@ class Solution:
     totalTimer = Timer()
 
     def minDistance(self, word1: str, word2: str) -> int:
-        def generateMatrix(word1: str, word2: str) -> list[list[int]]:
-            matrix = list()
-            
-            for i in range(0, len(word1)+1):
-                if i == 0:
-                    matrix.append([j for j in range(0, len(word2)+1)])
-                else:
-                    matrix.append([i])
-                    for j in range(1, len(word2)+1):
-                        if word1[i-1] != word2[j-1]:
-                            matrix[i].append(min([matrix[i-1][j]+1, matrix[i][j-1]+1, matrix[i-1][j-1]+1]))
-                        else: 
-                            matrix[i].append(matrix[i-1][j-1])
-                        
-            return matrix
-        return generateMatrix(word1, word2)[len(word1)][len(word2)]
+        if not word1 and not word2:
+            return 0
+        elif not word1:
+            return len(word2)
+        elif not word2:
+            return len(word1)
+        
+        prev_row = [j for j in range(0, len(word2)+1)]
+        word1_len = len(word1)
+        
+        for i in range(1, word1_len+1):
+            new_row = [i]
+            for j in range(1, len(word2)+1):
+                if word1[i-1] != word2[j-1]:
+                    new_row.append(min([prev_row[j]+1, new_row[j-1]+1, prev_row[j-1]+1]))
+                else: 
+                    new_row.append(prev_row[j-1])
+            prev_row = new_row
+        
+        return prev_row[-1]
 
 if __name__ == "__main__":
     s = Solution()

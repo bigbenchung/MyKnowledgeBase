2c0fa70facbf459c9799968339c8b57dc9f52c09
bigbenchung <ansonchung2012@gmail.com>
2024-04-07 19:01:13 +0800

    Finished Leetcode 678

---
+++ Leetcode/678.py
@@ -0,0 +1,28 @@
+from Timer import Timer
+
+class Solution:
+
+    totalTimer = Timer()
+
+    def checkValidString(self, s: str) -> bool:
+        if s[0] == ")" or s[-1] == "(":
+            return False
+        
+        forward_counter = 0
+        backward_counter = 0
+
+        for i in range(0, len(s)):
+            forward_counter += 1 if s[i] in ("(", "*") else -1
+            backward_counter += 1 if s[len(s)-i-1] in (")", "*") else -1
+            if forward_counter < 0 or backward_counter < 0:
+                return False
+        
+        return forward_counter >= 0 and backward_counter >= 0
+
+if __name__ == "__main__":
+    s = Solution()
+    
+    string = "(*)"
+    
+    print(s.checkValidString(string)) 
+    print(s.totalTimer.getTotalTime())
